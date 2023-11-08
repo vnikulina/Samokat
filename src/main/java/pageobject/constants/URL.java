4c806eefe2196d3b316fbde050e4c3ea8d0180a0
Valeria Nikulina <valeriya.nikulina@gmail.com>
2023-11-09 02:18:16 +0300

    Reworked MakingOrder Parametrization

---
+++ src/main/java/pageobject/constants/URL.java
@@ -0,0 +1,6 @@
+package pageobject.constants;
+
+public class URL {
+    public final static String HOME_PAGE = "https://qa-scooter.praktikum-services.ru/";
+    public final static String ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/order";
+}
